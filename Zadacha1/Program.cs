c3389c68fca5180e33f48add02a8f68bd59d0540
Stanyslav <stanyslavsky33@mail.ru>
2022-08-20 20:02:39 +0700

    zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,31 @@
+﻿ //Программа, на вход принимающая два числа (N),
+ //а на выходе показывает какое число большее, а какое меньшее.
+
+   int num;
+   int num2;                                                                                                                                                        //Ввод данных
+
+ // Ввод данных
+  Console.Write("Введите число первое ");
+  num = Convert.ToInt32(Console.ReadLine());
+
+  Console.Write("Введите число второе ");
+  num2= Convert.ToInt32(Console.ReadLine());
+            bool max0 = num > num2;
+            bool max1 = num == num2;
+            bool max2 = num < num2;
+ 
+ // Вычисление большего числа из двух
+            if (max0)
+            {
+                Console.WriteLine("Число первое больше чем второе");
+            }
+            else if (max1)
+            {
+                Console.WriteLine("Числа первое и второе равны");
+            }
+            else if (max2)
+            {
+                Console.WriteLine("Число первое меньше чем второе");
+            }
+// Вводим заданые цифры, получаем результат.
+
